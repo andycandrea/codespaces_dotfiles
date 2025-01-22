cf720a3372ff60a7f2cca02f9e5f103d2566cdca
Andy Andrea <aandrea@panoramaed.com>
2025-01-22 13:35:22 -0500

    Fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Copying dotfiles"
+CODESPACES_HOME="/workspaces/.codespaces/.persistedshare/dotfiles"
+BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
+BREWFILE_PATH="${CODESPACES_HOME}/Brewfile"
+LOCAL_CONFIG="${HOME}/.zshenv.local"
+
+echo "Copying dotfiles from ${CODESPACES_HOME} to ${HOME}"
 cp "${CODESPACES_HOME}/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${CODESPACES_HOME}/.gitconfig" "${HOME}/.gitconfig"
 cp "${CODESPACES_HOME}/.agignore" "${HOME}/.agignore"
@@ -16,20 +21,6 @@ cp "${CODESPACES_HOME}/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${CODESPACES_HOME}/.zshenv" "${HOME}/.zshenv"
 cp "${CODESPACES_HOME}/.zshrc" "${HOME}/.zshrc"
 
-echo "Installing brew"
-CODESPACES_HOME="/workspaces/.codespaces/.persistedshare/dotfiles"
-BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
-BREWFILE_PATH="${CODESPACES_HOME}/Brewfile"
-/bin/bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo "Installing brew packages"
-${BREW_BIN} bundle install --file="${BREWFILE_PATH}"
-
-echo "Install vim-plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
-       --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 echo "Copying over vim config"
 mkdir -p ${HOME}/.config/nvim
 cp nvimrc ${HOME}/.config/nvim/init.vim
@@ -39,9 +30,8 @@ echo "Changing shell to zsh"
 sudo chsh --shell /usr/bin/zsh codespace
 
 echo "Setting up local zsh config"
-local_config="${HOME}/.zshenv.local"
-if [ ! -e $local_config ]; then
-  cat > $local_config <<EOF
+if [ ! -e $LOCAL_CONFIG ]; then
+  cat > $LOCAL_CONFIG <<EOF
 export PANORAMA_TOP=/workspaces
 SCHOOL_SUPPLIES_HOME="${PANORAMA_TOP}/school-supplies"
 source "/usr/local/share/chruby/chruby.sh"
@@ -49,3 +39,14 @@ source "/usr/local/share/chruby/auto.sh"
 source "${SCHOOL_SUPPLIES_HOME}/bin/shell_includes.sh"
 EOF
 fi
+
+echo "Installing brew"
+/bin/bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing brew packages"
+${BREW_BIN} bundle install --file="${BREWFILE_PATH}"
+
+echo "Install vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+       --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
