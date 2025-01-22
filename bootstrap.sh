2b78ff2b133ec9a39c697bc2db028b473b620b2e
Andy Andrea <aandrea@panoramaed.com>
2025-01-22 12:04:06 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,24 +8,32 @@ ${BREW_BIN} bundle install --file="${BREWFILE_PATH}"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp .tmux.conf ~
-cp .gitconfig ~
-
-# Setup git completions for bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-echo "source ~/.git-completion.bash" >> ~/.bashrc
+cp "${CODESPACES_HOME}.tmux.conf" "${HOME}/.tmux.conf"
+cp "${CODESPACES_HOME}.gitconfig" "${HOME}/.gitconfig"
+cp "${CODESPACES_HOME}.agignore" "${HOME}/.agignore"
+cp "${CODESPACES_HOME}.aliases" "${HOME}/.aliases"
+cp "${CODESPACES_HOME}.gemrc" "${HOME}/.gemrc"
+cp "${CODESPACES_HOME}.gitconfig" "${HOME}/.gitconfig"
+cp "${CODESPACES_HOME}.gitignore" "${HOME}/.gitignore"
+cp "${CODESPACES_HOME}.gitmessage" "${HOME}/.gitmessage"
+cp "${CODESPACES_HOME}.nvimrc.bundles" "${HOME}/.nvimrc.bundles"
+cp "${CODESPACES_HOME}.pryrc" "${HOME}/.pryrc"
+cp "${CODESPACES_HOME}.psqlrc" "${HOME}/.psqlrc"
+cp "${CODESPACES_HOME}.tmux.conf" "${HOME}/.tmux.conf"
+cp "${CODESPACES_HOME}.zshenv" "${HOME}/.zshenv"
+cp "${CODESPACES_HOME}.zshrc" "${HOME}/.zshrc"
 
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Bring in our custom neovim config
-mkdir -p ~/.config/nvim
-cp nvimrc ~/.config/nvim/init.vim
-cp nvim.coc-settings.json ~/.config/nvim/coc-settings.json
+mkdir -p ${HOME}/.config/nvim
+cp nvimrc ${HOME}/.config/nvim/init.vim
+cp nvim.coc-settings.json ${HOME}/.config/nvim/coc-settings.json
 sudo chsh --shell "/usr/bin/zsh" "$(whoami)"
 
-local_config="~/.zshenv.local"
+local_config="${HOME}/.zshenv.local"
 if [ ! -e $local_config ]; then
   cat > $local_config <<EOF
 export PANORAMA_TOP=/workspaces
