ca11b406b5ca97f1bbc5c1b3dba8ec2a808c2fd5
Andy Andrea <aandrea@panoramaed.com>
2025-01-22 13:02:24 -0500

    Add log statements to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install build-essential
-
-CODESPACES_HOME="/workspaces/.codespaces/.persistedshare/dotfiles"
-BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
-BREWFILE_PATH="${CODESPACES_HOME}/Brewfile"
-/bin/bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-${BREW_BIN} bundle install --file="${BREWFILE_PATH}"
-
+echo "Copying dotfiles"
 cp "${CODESPACES_HOME}/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${CODESPACES_HOME}/.gitconfig" "${HOME}/.gitconfig"
 cp "${CODESPACES_HOME}/.agignore" "${HOME}/.agignore"
@@ -23,22 +16,33 @@ cp "${CODESPACES_HOME}/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${CODESPACES_HOME}/.zshenv" "${HOME}/.zshenv"
 cp "${CODESPACES_HOME}/.zshrc" "${HOME}/.zshrc"
 
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+echo "Installing brew"
+CODESPACES_HOME="/workspaces/.codespaces/.persistedshare/dotfiles"
+BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
+BREWFILE_PATH="${CODESPACES_HOME}/Brewfile"
+/bin/bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing brew packages"
+${BREW_BIN} bundle install --file="${BREWFILE_PATH}"
+
+echo "Install vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+       --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Bring in our custom neovim config
+echo "Copying over vim config"
 mkdir -p ${HOME}/.config/nvim
 cp nvimrc ${HOME}/.config/nvim/init.vim
 cp nvim.coc-settings.json ${HOME}/.config/nvim/coc-settings.json
+
+echo "Changing shell to zsh"
 sudo chsh --shell /usr/bin/zsh codespace
 
+echo "Setting up local zsh config"
 local_config="${HOME}/.zshenv.local"
 if [ ! -e $local_config ]; then
   cat > $local_config <<EOF
 export PANORAMA_TOP=/workspaces
-# These files are sourced in our devcontainer's on-create.sh, but that's for
-# bash.
 SCHOOL_SUPPLIES_HOME="${PANORAMA_TOP}/school-supplies"
 source "/usr/local/share/chruby/chruby.sh"
 source "/usr/local/share/chruby/auto.sh"
